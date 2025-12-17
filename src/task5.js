d294ee2893f1de58f4c8eec9d3e8ca718a9bebde
Andrea Meneses <andrea.elizabeth.meneses.g@gmail.com>
2025-12-17 16:04:53 -0600

    Actividad terminada hasta las 6

--- src/task5.js
+++ src/task5.js
@@ -2,7 +2,12 @@ const arr0 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 
 // Type your code below this line!
 
-
+console.log(arr.join(','));
+// arr.join(',') convierte el array en una cadena de texto
+// donde cada número se separa solo por comas, sin espacios
+// Ejemplo: [1,2,3] se convierte en "1,2,3"
+/* Sin parámetro: separa con comas Y espacios
+arr.join(); // Resultado: "1, 2, 3, 4, 5, ..." ← CON espacios*/
 
 // Type your code above this line!
 console.log("hola")
--- src/task6.js
+++ src/task6.js
@@ -3,6 +3,10 @@ const arr2 = ["world"]
 
 // Type your code below this line!
 
-
+// concat() une dos arrays en orden: primero arr1, luego arr2
+// Creamos un nuevo array con la unión de ambos
+const nuevoArray = arr1.concat(arr2);
+// Imprimimos el nuevo array resultante
+console.log(nuevoArray);
 
 // Type your code above this line!
